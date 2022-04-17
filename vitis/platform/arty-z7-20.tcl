a364d8c581e7375724b1b078dbfb551d1e7cc7ac
Nik Mohamad Aizuddin <nick@extra2000.io>
2022-04-17 22:29:05 +0800

    feat: initial implementations

---
+++ vitis/platform/arty-z7-20.tcl
@@ -0,0 +1,8 @@
+platform create -name {arty-z7-20}\
+-hw {../../vivado/run/vitis_helloworld/arty_z7_20_wrapper.xsa}\
+-proc {ps7_cortexa9_0} -os {standalone} -out {../../workspace/}
+
+platform write
+platform generate -domains
+platform active {arty-z7-20}
+platform generate
