0dc945067e96d7171fc2a2cad4ebf485255c60bc
Nik Mohamad Aizuddin <nick@extra2000.io>
2022-04-22 18:52:18 +0800

    fix(vitis): fix invalid path

--- vitis/platform/arty-z7-20.tcl
+++ vitis/platform/arty-z7-20.tcl
@@ -1,6 +1,6 @@
 platform create -name {arty-z7-20}\
--hw {../../vivado/run/vitis_helloworld/arty_z7_20_wrapper.xsa}\
--proc {ps7_cortexa9_0} -os {standalone} -out {../../workspace/}
+-hw {../vivado/run/vitis_helloworld/arty_z7_20_wrapper.xsa}\
+-proc {ps7_cortexa9_0} -os {standalone} -out {../workspace/}
 
 platform write
 platform generate -domains
